b46ad5543ddd75e1f494d16a8b023fb88c538546
ArdiOsmani <ardiosmani02@gmail.com>
2024-02-08 18:21:28 +0100

    script done

---
+++ Projects/[JavaScript] Dashboard/script.js
@@ -0,0 +1,7 @@
+import { initializeSidebar} from './scripts/sidebar.js';
+import {notification} from './scripts/notifications.js';
+import {listing} from './scripts/listing.js';
+
+notification();
+initializeSidebar();
+listing();
