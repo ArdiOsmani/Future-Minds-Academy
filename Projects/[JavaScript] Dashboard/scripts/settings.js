22f2827fd0e244118582cf022924dbd93142ba6a
ArdiOsmani <ardiosmani02@gmail.com>
2024-02-12 19:04:06 +0100

    script done

--- Projects/[JavaScript] Dashboard/scripts/settings.js
+++ Projects/[JavaScript] Dashboard/scripts/settings.js
@@ -1,4 +1,39 @@
 export function settings() {
 
-    
+    let setGreen = document.getElementById('set-green');
+    let setPurple = document.getElementById('set-purple');
+    let setBlue = document.getElementById('set-blue');
+    let setOrange = document.getElementById('set-orange');
+
+    setGreen.addEventListener('click', ()=>{
+        setGreen.classList.add('with-border');
+        setPurple.classList.remove('with-border');
+        setBlue.classList.remove('with-border');
+        setOrange.classList.remove('with-border');
+        document.documentElement.style.setProperty('--active-color', 'green');
+    })
+
+    setPurple.addEventListener('click', ()=>{
+        setGreen.classList.remove('with-border');
+        setPurple.classList.add('with-border');
+        setBlue.classList.remove('with-border');
+        setOrange.classList.remove('with-border');
+        document.documentElement.style.setProperty('--active-color', 'purple');
+    })
+
+    setBlue.addEventListener('click', ()=>{
+        setGreen.classList.remove('with-border');
+        setPurple.classList.remove('with-border');
+        setBlue.classList.add('with-border');
+        setOrange.classList.remove('with-border');
+        document.documentElement.style.setProperty('--active-color', 'lightblue');
+    })
+
+    setOrange.addEventListener('click', ()=>{
+        setGreen.classList.remove('with-border');
+        setPurple.classList.remove('with-border');
+        setBlue.classList.remove('with-border');
+        setOrange.classList.add('with-border');
+        document.documentElement.style.setProperty('--active-color', 'orangered');
+    })
 }
