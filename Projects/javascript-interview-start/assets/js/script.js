729d7708ef976fd45df29735eded763cff70c6c1
ArdiOsmani <ardiosmani02@gmail.com>
2024-02-21 22:26:12 +0100

    script done

--- Projects/javascript-interview-start/assets/js/script.js
+++ Projects/javascript-interview-start/assets/js/script.js
@@ -1,6 +1,11 @@
 const movieGernes = document.querySelector('.movieGernes');
 const listMovies = document.querySelector('.movielist');
-let index=0
+const movieDetailsPage = document.querySelector('.movie-details-page');
+let index = 0
+
+movieGernes.addEventListener('click', ()=>{
+    listAllMovies(movieGernes.value);
+});
 
 function buildGenres() {
     let movieList = "";
@@ -15,11 +20,11 @@ function buildGenres() {
 
 function listAllMovies(index) {
     let allListMovies = "";
-    
+
     for (let i = 0; i < allMovies[index].movies.length; i++) {
         let movie = allMovies[index].movies[i];
 
-        allListMovies += `<li>
+        allListMovies += `<li onclick="revealMovie(${index}, ${i})">
         <h4>${movie.title}</h4>
         <img src="assets/img/${movie.thumb}" alt="${movie.title}">
         <p class="description">${movie.desc}</p>
@@ -32,6 +37,47 @@ function listAllMovies(index) {
 
     listMovies.innerHTML = allListMovies;
 }
+
+function revealMovie(index, i) {
+    let movie = allMovies[index].movies[i];
+    let movieShow = `
+    <h1>${movie.title} (2023)</h1>
+    <h4>Date: ${movie.date} | Length: ${movie.length}min</h4>
+    <div class="container">
+    ${movie.trailer}
+    </div>
+
+    <h4>Gerard Butler, Mike Colter, Tony Goldwyn, Yoson An</h4>
+    <p>${movie.desc}</p>
+    `;
+
+
+
+    movieDetailsPage.innerHTML = movieShow;
+}
+
+
+function changeMovieInterval(){
+
+
+    allMovies[movieGernes.value].movies.forEach(movie=>{
+        setTimeout(()=>{
+            movieDetailsPage.innerHTML = `
+            <h1>${movie.title} (2023)</h1>
+            <h4>Date: ${movie.date} | Length: ${movie.length}min</h4>
+            <div class="container">
+            ${movie.trailer}
+            </div>
+        
+            <h4>Gerard Butler, Mike Colter, Tony Goldwyn, Yoson An</h4>
+            <p>${movie.desc}</p>
+            `;
+            
+        }, 500)
+        console.log('hello');
+    })  
+}
+
 
 listAllMovies(0);
 
