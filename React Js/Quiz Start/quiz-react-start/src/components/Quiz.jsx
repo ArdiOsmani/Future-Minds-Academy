4b82612309d0919290d97ae9a52ce27a47593ae8
ArdiOsmani <ardiosmani02@gmail.com>
2024-06-03 22:47:23 +0200

    project

---
+++ React Js/Quiz Start/quiz-react-start/src/components/Quiz.jsx
@@ -0,0 +1,39 @@
+import Questions from '../assets/js/questions.js'
+import { useState } from 'react';
+
+export default function Quiz(){
+
+    const [userAnswers, setUserAnswers] = useState([]);
+
+    const activeQuestionIndex = userAnswers.length;
+
+    function handleAnswer(selectedAnswer){
+        setUserAnswers(
+            (previusAnswer) =>{
+                return [...previusAnswer, selectedAnswer]
+            }
+        );
+    }
+
+    return(
+        <>
+        <main>
+            <div id="quiz">
+                <div id="question">
+                    <h2>{Questions[activeQuestionIndex].text}</h2>
+                    <ul id="answers">
+
+                        {
+                            Questions[activeQuestionIndex].answers.map((answer)=>
+                                <li key={answer} className="answer">
+                                    <button onClick={()=>handleAnswer(answer)}>{answer}</button>
+                                </li>
+                            )
+                        }
+                    </ul>
+                </div>
+            </div>
+        </main>
+        </>
+    );
+}
