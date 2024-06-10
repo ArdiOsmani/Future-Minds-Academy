0936217617a657f3cca606f3fa9ae9f5bc354e4c
ArdiOsmani <ardiosmani02@gmail.com>
2024-06-10 19:47:15 +0200

    ...

---
+++ React Js/Hooks example/Hooks/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Counter from './views/Counter'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Welcome</h1>
+      <Counter/>
+    </>
+  )
+}
+
+export default App
---
+++ React Js/Hooks example/Hooks/src/views/Counter.jsx
@@ -0,0 +1,10 @@
+import Nav from '../components/nav/nav.jsx'
+
+export default function Counter(){
+
+    return(
+        <>
+            <Nav />
+        </>
+    );
+}
