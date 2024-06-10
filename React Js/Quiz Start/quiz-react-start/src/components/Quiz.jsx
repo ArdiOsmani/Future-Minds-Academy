05057f33f11851b5cf9816af50d53be9c6446922
ArdiOsmani <ardiosmani02@gmail.com>
2024-06-10 19:12:38 +0200

    fixes

--- React Js/Quiz Start/quiz-react-start/src/components/Quiz.jsx
+++ React Js/Quiz Start/quiz-react-start/src/components/Quiz.jsx
@@ -1,11 +1,14 @@
 import Questions from '../assets/js/questions.js'
 import { useState } from 'react';
+import QuizTimer from './QuizTimer.jsx';
 
 export default function Quiz(){
 
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+
+    const isQuizComplete = activeQuestionIndex === Questions.length;
 
     function handleAnswer(selectedAnswer){
         setUserAnswers(
@@ -15,16 +18,30 @@ export default function Quiz(){
         );
     }
 
+    if(isQuizComplete){
+        return(
+            <>
+                <div id="summary">
+                    <img src="/src/assets/quiz-complete.png" alt="Quiz Completed"/>
+                    <h2>Quiz Complete</h2>
+                </div>
+            </>
+        );
+    }
+
+    const shuffleAnswers = [...Questions[activeQuestionIndex].answers.sort((a,b) => 0.5 - Math.random())]
+
     return(
         <>
         <main>
             <div id="quiz">
                 <div id="question">
+                    <QuizTimer/>
                     <h2>{Questions[activeQuestionIndex].text}</h2>
                     <ul id="answers">
 
                         {
-                            Questions[activeQuestionIndex].answers.map((answer)=>
+                            shuffleAnswers.map((answer)=>
                                 <li key={answer} className="answer">
                                     <button onClick={()=>handleAnswer(answer)}>{answer}</button>
                                 </li>
